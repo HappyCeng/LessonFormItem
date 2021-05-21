4f7d74543731268fb47f7cb6a9418af5ce18bfa9
éHappyCeng <mutlu.m38@gmail.com>
2021-05-21 22:50:49 +0300

    Add project files.

---
+++ LessonFormItem/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace LessonFormItem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickMe = new System.Windows.Forms.Button();
+            this.btnClickMe2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.cBoxNames = new System.Windows.Forms.ComboBox();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.Location = new System.Drawing.Point(12, 12);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(75, 23);
+            this.btnClickMe.TabIndex = 0;
+            this.btnClickMe.Text = "Bana Tıkla";
+            this.btnClickMe.UseVisualStyleBackColor = true;
+            this.btnClickMe.Click += new System.EventHandler(this.btnClickMe_Click);
+            // 
+            // btnClickMe2
+            // 
+            this.btnClickMe2.Location = new System.Drawing.Point(93, 12);
+            this.btnClickMe2.Name = "btnClickMe2";
+            this.btnClickMe2.Size = new System.Drawing.Size(75, 23);
+            this.btnClickMe2.TabIndex = 1;
+            this.btnClickMe2.Text = "Bana Tıkla 2";
+            this.btnClickMe2.UseVisualStyleBackColor = true;
+            this.btnClickMe2.Click += new System.EventHandler(this.btnClickMe2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(226, 18);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(226, 42);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(226, 66);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Ali",
+            "Veli",
+            "49",
+            "50"});
+            this.checkedListBox1.Location = new System.Drawing.Point(205, 161);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 5;
+            // 
+            // cBoxNames
+            // 
+            this.cBoxNames.FormattingEnabled = true;
+            this.cBoxNames.Location = new System.Drawing.Point(372, 42);
+            this.cBoxNames.Name = "cBoxNames";
+            this.cBoxNames.Size = new System.Drawing.Size(121, 21);
+            this.cBoxNames.TabIndex = 6;
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDisplay.Location = new System.Drawing.Point(13, 49);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(60, 24);
+            this.lblDisplay.TabIndex = 7;
+            this.lblDisplay.Text = "label1";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(12, 76);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 8;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(372, 126);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(372, 150);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 401);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.cBoxNames);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnClickMe);
+            this.Controls.Add(this.btnClickMe2);
+            this.Name = "Form1";
+            this.Text = "Happy Form\'s";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickMe;
+        private System.Windows.Forms.Button btnClickMe2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ComboBox cBoxNames;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ LessonFormItem/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LessonFormItem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Merhaba Dünya");
+            cBoxNames.Items.Add("Samet");
+            cBoxNames.Items.Add("Semih");
+            cBoxNames.Items.Add("Mustafa");
+            lblDisplay.Text = "Kimse seçilmedi.";
+        }
+
+        private void btnClickMe_Click(object sender, EventArgs e)
+        {
+            if(cBoxNames.Text == "" && txtAge.Text == "")
+            {
+                MessageBox.Show("Boş bırakma kardeşim!");
+            }
+            else if(cBoxNames.Text == "")
+            {
+                MessageBox.Show("Kişi seçiniz!");
+            }
+            else if (txtAge.Text == "")
+            {
+                MessageBox.Show("Yaşını giriniz!");
+            }
+            else
+            {
+                lblDisplay.Text = cBoxNames.Text + " yaşı " + txtAge.Text;
+            }
+            
+        }
+
+        private void btnClickMe2_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "Kimse seçilmedi.";
+            cBoxNames.Text = "";
+            txtAge.Text = "";
+        }
+    }
+}
